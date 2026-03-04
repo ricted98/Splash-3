fb484ee06be1bae01e5d96c619b3c20e3803b2ea
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2026-03-04 16:42:50 +0100

    fft: add RTEMS porting

---
+++ codes/kernels/fft/posix_init.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <rtems.h>
+
+int main(int argc, char **argv);
+
+void *POSIX_Init(void *arg)
+{
+    int argc = 3;
+    char *argv[] = { "fft", "-p2", "-m16", NULL };
+    int rc = main(argc, argv);
+    exit(rc);
+    return NULL; // not reached
+}
+
+/* Drivers needed */
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+
+/* POSIX configuration */
+#define CONFIGURE_POSIX_APPLICATION
+#define CONFIGURE_MAXIMUM_PROCESSORS 2
+#define CONFIGURE_MAXIMUM_POSIX_THREADS 2
+#define CONFIGURE_POSIX_INIT_THREAD_TABLE
+#define CONFIGURE_USE_IMFS_AS_BASE_FILESYSTEM
+#define CONFIGURE_MAXIMUM_FILE_DESCRIPTORS 32
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
